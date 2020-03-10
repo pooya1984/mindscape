78b95c1f8f0fd451d319c7813fab73ff5e1cd6b1
pooya <pooya.nodehi@gmail.com>
2020-03-10 13:56:11 +0100

    update posts UI

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -31,7 +31,7 @@ const PostItem = ({
           <button
             onClick={() => addLike(_id)}
             type="button"
-            className="btn btn-light"
+            className="btn btn-outline-secondary"
           >
             <i className="fas fa-thumbs-up" />{" "}
             <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
@@ -39,12 +39,12 @@ const PostItem = ({
           <button
             onClick={() => removeLike(_id)}
             type="button"
-            className="btn btn-light"
+            className="btn btn-outline-secondary"
           >
             <i className="fas fa-thumbs-down" />
           </button>
-          <Link to={`/posts/${_id}`} className="btn btn-primary">
-            Comments{" "}
+          <Link to={`/posts/${_id}`} className="btn btn-outline-secondary">
+            <i class="far fa-comments" />{" "}
             {comments.length > 0 && (
               <span className="comment-count">{comments.length}</span>
             )}
